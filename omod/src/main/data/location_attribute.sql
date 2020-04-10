12c3b29b3731136fa91cab576e55014403b8fb89
Subashini Senthil <subashini@Subashinis-MacBook-Pro.local>
2020-04-10 09:29:14 +0530

    adding data files

---
+++ omod/src/main/data/location_attribute.sql
@@ -0,0 +1,3 @@
+INSERT INTO `location_attribute` (`location_attribute_id`, `location_id`, `attribute_type_id`, `value_reference`, `uuid`, `creator`, `date_created`, `changed_by`, `date_changed`, `voided`, `voided_by`, `date_voided`, `void_reason`)
+VALUES
+	(1, 6, 1, 'Enterprise', 'e0c2422c-f74e-4e65-9822-813862158b02', 1, '2020-04-01 00:00:00', NULL, NULL, 0, NULL, NULL, NULL);
